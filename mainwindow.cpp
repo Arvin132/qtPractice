27903f54bd33bff8bd79b72cd0dc585de1d3210f
Arvin132 <a29asgha@uwaterloo.ca>
2023-01-02 19:27:54 -0500

    inital commit

---
+++ mainwindow.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+    this->setCentralWidget(ui->textEdit_2);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
